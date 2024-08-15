4c97e3796706c9ab1a57ea0aa30dd6fdd2f95884
Vishaylinp <vishaylinparshotam@gmail.com>
2024-08-15 14:15:10 +0000

    log parse

---
+++ 0x03-log_parsing/0-stats.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""log parsing"""
+import sys
+
+
+status_codes = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0,
+                "404": 0, "405": 0, "500": 0}
+size = 0
+counter = 0
+
+try:
+    for line in sys.stdin:
+        l_list = line.split(" ")
+
+        if len(l_list) > 4:
+            status_code = l_list[-2]
+            file_size = int(l_list[-1])
+
+        if status_code in status_codes.keys():
+            status_codes[status_code] += 1
+
+        size += file_size
+        counter += 1
+
+    if counter == 10:
+        counter = 0
+        print("File size: {}".format(size))
+
+        for k, v in sorted(status_codes.items()):
+            if v != 0:
+                print("{}.{}".format(k, v))
+
+except Exception as err:
+    pass
+
+finally:
+    print("File size:{}".format(size))
+    for k, v in sorted(status_codes.items()):
+        if v != 0:
+            print("{}: {}".format(k, v))
