e4e35f27e9a6093abe63e3685616f14732ad1009
Vishaylinp <vishaylinparshotam@gmail.com>
2024-09-05 11:19:36 +0000

    starwars

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,31 @@
+#!/usr/bin/node
+
+const request = require("request")
+
+const movie_id = process.argv[2];
+const movie_url = `https://swapi-api.alx-tools.com/api/films` + movie_id;
+
+function request_sent(character_list, index) {
+  if (character_list.length === index) {
+    return;
+  }
+
+  request(character_list[index], (error, response, body) => {
+
+    if (error) {
+      console.log(error);
+    } else {
+      console.log(JSON.parse(body).name);
+      request_sent(character_list, index + 1);
+    }
+  });
+}
+
+request(movie_url, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else {
+    const character_list = JSON.parse(body).characters;
+    request_sent(character_list, 0);
+  }
+});
